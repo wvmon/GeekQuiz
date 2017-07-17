60eefce1708e9819664be12cc7c59cdb9fd07b44
William Montesedoca <wvmon360@gmail.com>
2017-07-16 23:40:12 -0600

    Style was added

--- GeekQuiz/App_Start/BundleConfig.cs
+++ GeekQuiz/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace GeekQuiz
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Flip.css"));
         }
     }
 }
